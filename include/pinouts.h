d36123193a64bb0a2c345c40150d1d358d5a118c
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2024-08-31 13:42:59 +0500

    base

---
+++ include/pinouts.h
@@ -0,0 +1,15 @@
+#ifndef PINOUTS_H
+#define PINOUT_H 
+
+
+#define DHTPIN 2      // fill pin TODO:
+#define DHTTYPE DHT22 // DHT 22 (AM2302)
+
+#define ROTARY_ENCODER_A_PIN 32
+#define ROTARY_ENCODER_B_PIN 21
+#define ROTARY_ENCODER_BUTTON_PIN 25
+#define ROTARY_ENCODER_VCC_PIN -1 /* 27 put -1 of Rotary encoder Vcc is connected directly to 3,3V; else you can use declared output pin for powering rotary encoder */
+
+
+
+#endif 
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include "mMenu.h"
-// constants
-#define DHTPIN 2      // fill pin TODO:
-#define DHTTYPE DHT22 // DHT 22 (AM2302)
+#include "pinouts.h"
+#include "AiEsp32RotaryEncoder.h"
 
+// constants
+#define ROTARY_ENCODER_STEPS 4
 
 // INITIALIZETD
 DHT dht(DHTPIN, DHTTYPE);
-
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
 
 // Structs
 struct sensor
@@ -33,10 +34,32 @@ void UpdateSensor(sensor *sensor)
   }
 }
 
+void updateEncoder()
+{
+  if (rotaryEncoder.encoderChanged())
+  {
+    Serial.println(rotaryEncoder.readEncoder());
+  }
+  if (rotaryEncoder.isEncoderButtonClicked())
+  {
+    Serial.println("button pressed");
+  }
+}
+
+void IRAM_ATTR readEncoderISR()
+{
+  rotaryEncoder.readEncoder_ISR();
+}
+
 void setup()
 {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   menu.initialize(mainMenu);
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+  rotaryEncoder.setBoundaries(0, 1000, false); // minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+  rotaryEncoder.setAcceleration(250);
 }
 
 void loop()
