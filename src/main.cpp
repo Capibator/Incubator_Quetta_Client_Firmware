8a5dfac7df3e0e3edf7fb203755f0680879e7846
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2024-09-01 10:00:15 +0500

    encoder now working with menu

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,15 @@
 
 // constants
 #define ROTARY_ENCODER_STEPS 2
-bool dir; 
+int dir;
+#define IDLE 0
+#define UP 1
+#define DOWN 2
+#define ENTER 3
+#define BACK 4
+
+#define longPressAfterMiliseconds  500  //how long čong press shoud be.
+
 // INITIALIZETD
 DHT dht(DHTPIN, DHTTYPE);
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
@@ -19,14 +27,6 @@ struct sensor
   float curHum;
   unsigned long curTime, prevTimeSens;
   const uint16_t updateTime = 2000;
-};
-
-struct encoder
-{
-  int clk;
-  int data;
-  int sw;
-  unsigned long prevTimeEnc; // keeps track of encoder time.
 };
 
 // update the crucial sensor every 2 seconds.. well updates all sensors.
@@ -42,36 +42,76 @@ void UpdateSensor(sensor *sensor)
   }
 }
 
+
+void on_button_long_click() {
+  Serial.print("button LONG press ");
+  dir = BACK; 
+}
+
+void handle_rotary_button() {
+  static unsigned long lastTimeButtonDown = 0;
+  static bool wasButtonDown = false;
+
+  bool isEncoderButtonDown = rotaryEncoder.isEncoderButtonDown();
+  //isEncoderButtonDown = !isEncoderButtonDown; //uncomment this line if your button is reversed
+
+  if (isEncoderButtonDown) {
+    Serial.print("+");  //REMOVE THIS LINE IF YOU DONT WANT TO SEE
+    if (!wasButtonDown) {
+      //start measuring
+      lastTimeButtonDown = millis();
+    }
+    //else we wait since button is still down
+    wasButtonDown = true;
+    return;
+  }
+
+  //button is up
+  if (wasButtonDown) {
+    Serial.println("");  //REMOVE THIS LINE IF YOU DONT WANT TO SEE
+    //click happened, lets see if it was short click, long click or just too short
+    if (millis() - lastTimeButtonDown >= longPressAfterMiliseconds) {
+      on_button_long_click();
+    }
+  }
+  wasButtonDown = false;
+}
+
 void updateEncoder()
 {
-  static int prevVal = 0;  // Make prevVal static to retain its value between function calls
+  static int prevVal = 0; // Make prevVal static to retain its value between function calls
   int val;
   dir;
+
+
+  if (rotaryEncoder.isEncoderButtonClicked())
+  {
+    
+    dir = ENTER;
+    Serial.println("button pressed");
+  }
+
 
   if (rotaryEncoder.encoderChanged())
   {
     val = rotaryEncoder.readEncoder();
 
     // Check for changes in encoder value and determine direction
-    if (val != prevVal)  // Only proceed if the value has changed
+    if (val != prevVal) // Only proceed if the value has changed
     {
-      if(val == 100 || val == 0) return; 
+      if (val == 100 || val == 0)
+        return;
       // Determine direction
-      dir = (val > prevVal) ? true : false;  // dir is true if moving right
+      dir = (val > prevVal) ? UP : DOWN; //
 
       // Print direction
-      if (dir)
-        Serial.println("left");
+      if (dir == UP)
+        Serial.println("UP");
       else
-        Serial.println("right");
+        Serial.println("DOWN");
 
-      prevVal = val;  // Update prevVal with the new value
+      prevVal = val; // Update prevVal with the new value
     }
-  }
-
-  if (rotaryEncoder.isEncoderButtonClicked())
-  {
-    Serial.println("button pressed");
   }
 }
 
@@ -92,8 +132,8 @@ void setup()
 
 void loop()
 {
-  updateEncoder(); 
-  if (!Serial.available())
-    return;
-  char command = Serial.read();
+  updateEncoder();
+  handle_rotary_button();
+  processMenuCommand(menu, dir, UP, DOWN, ENTER, BACK);
+  dir = IDLE; 
 }
