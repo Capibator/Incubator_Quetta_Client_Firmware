31693e662f1cbab85b5e695ec7baa9d2df477ff8
manhoosbilli1 <shoaib.mustafa7@hotmail.com>
2024-09-01 09:28:31 +0500

    basic working

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 // constants
 #define ROTARY_ENCODER_STEPS 2
-
+bool dir; 
 // INITIALIZETD
 DHT dht(DHTPIN, DHTTYPE);
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, -1, ROTARY_ENCODER_STEPS);
@@ -19,6 +19,14 @@ struct sensor
   float curHum;
   unsigned long curTime, prevTimeSens;
   const uint16_t updateTime = 2000;
+};
+
+struct encoder
+{
+  int clk;
+  int data;
+  int sw;
+  unsigned long prevTimeEnc; // keeps track of encoder time.
 };
 
 // update the crucial sensor every 2 seconds.. well updates all sensors.
@@ -36,10 +44,31 @@ void UpdateSensor(sensor *sensor)
 
 void updateEncoder()
 {
+  static int prevVal = 0;  // Make prevVal static to retain its value between function calls
+  int val;
+  dir;
+
   if (rotaryEncoder.encoderChanged())
   {
-    Serial.println(rotaryEncoder.encoderSteps);
+    val = rotaryEncoder.readEncoder();
+
+    // Check for changes in encoder value and determine direction
+    if (val != prevVal)  // Only proceed if the value has changed
+    {
+      if(val == 100 || val == 0) return; 
+      // Determine direction
+      dir = (val > prevVal) ? true : false;  // dir is true if moving right
+
+      // Print direction
+      if (dir)
+        Serial.println("left");
+      else
+        Serial.println("right");
+
+      prevVal = val;  // Update prevVal with the new value
+    }
   }
+
   if (rotaryEncoder.isEncoderButtonClicked())
   {
     Serial.println("button pressed");
@@ -54,12 +83,11 @@ void IRAM_ATTR readEncoderISR()
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   menu.initialize(mainMenu);
   rotaryEncoder.begin();
   rotaryEncoder.setup(readEncoderISR);
-  rotaryEncoder.setBoundaries(0, 1000, false); // minValue, maxValue, circleValues true|false (when max go to min and vice versa)
-  rotaryEncoder.setAcceleration(250);
+  rotaryEncoder.setBoundaries(0, 100, true); // minValue, maxValue, circleValues true|false (when max go to min and vice versa)
 }
 
 void loop()
@@ -68,5 +96,4 @@ void loop()
   if (!Serial.available())
     return;
   char command = Serial.read();
-  processMenuCommand(menu, command, UP, DOWN, ENTER, BACK);
 }
